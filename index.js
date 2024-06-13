37860b5af4067800dec2f9f18a2a40c464a7df43
HUkhan92 <hammadacg@gmail.com>
2024-06-13 21:16:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/user/bin/env node
+import inquirer from "inquirer";
+let user_ans = await inquirer.prompt([{
+        type: "input",
+        name: "words",
+        message: "please enter your sentence"
+    }]);
+let words_count = user_ans.words.trim().split(" ").length;
+console.log(`your sentence has ${words_count} words`);
